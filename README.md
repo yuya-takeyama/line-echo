94d04f53a9053191f2e6e36778300519e7d60251
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-04-09 01:56:18 +0900

    Deploy button

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# LINE echo bot
+# LINE echo 
 
-Almostly taken from http://qiita.com/masuidrive/items/1042d93740a7a72242a3
+Echo server for LINE BOT API
 
-Gomen-nasai
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+Mostly taken from http://qiita.com/masuidrive/items/1042d93740a7a72242a3
+
+[Article](http://qiita.com/yuya_takeyama/items/0660a59d13e2cd0b2516) (ja)
---
+++ app.rb
@@ -0,0 +1,33 @@
+# Mostly taken from http://qiita.com/masuidrive/items/1042d93740a7a72242a3
+
+require 'sinatra/base'
+require 'json'
+require 'rest-client'
+
+class App < Sinatra::Base
+  post '/linebot/callback' do
+    params = JSON.parse(request.body.read)
+
+    params['result'].each do |msg|
+      request_content = {
+        to: [msg['content']['from']],
+        toChannel: 1383378250, # Fixed  value
+        eventType: "138311608800106203", # Fixed value
+        content: msg['content']
+      }
+
+      endpoint_uri = 'https://trialbot-api.line.me/v1/events'
+      content_json = request_content.to_json
+
+      RestClient.proxy = ENV['FIXIE_URL'] if ENV['FIXIE_URL']
+      RestClient.post(endpoint_uri, content_json, {
+        'Content-Type' => 'application/json; charset=UTF-8',
+        'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+        'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+        'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+      })
+    end
+
+    "OK"
+  end
+end
